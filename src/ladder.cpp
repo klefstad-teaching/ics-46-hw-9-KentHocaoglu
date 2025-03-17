f9751cebefdf8a590bde8226a9394c3a3d80cc65
KentHocaoglu <khocaogl@uci.edu>
2025-03-17 04:17:01 +0000

    fixed verify ladder

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -75,12 +75,23 @@ void print_word_ladder(const vector<string>& ladder){
     }
 }
 
-// void verify_word_ladder() {
-//     for (size_t i = 1; i < ladder.size(); ++i) {
-//         if (!is_adjacent(ladder[i - 1], ladder[i])) {
-//             error(ladder[i - 1], ladder[i], "Bad Ladder");
-//             return;
-//         }
-//     }
-//     cout << "Good Ladder" << endl;
-// }
+#define my_assert(e) {cout << #e << ((e) ? " passed": " failed") << endl;}
+void verify_word_ladder() {
+
+    set<string> word_list;
+
+    load_words(word_list, "words.txt");
+
+    my_assert(generate_word_ladder("cat", "dog", word_list).size() == 4);
+
+    my_assert(generate_word_ladder("marty", "curls", word_list).size() == 6);
+
+    my_assert(generate_word_ladder("code", "data", word_list).size() == 6);
+
+    my_assert(generate_word_ladder("work", "play", word_list).size() == 6);
+
+    my_assert(generate_word_ladder("sleep", "awake", word_list).size() == 8);
+
+    my_assert(generate_word_ladder("car", "cheat", word_list).size() == 4);
+
+}
--- src/ladder_main.cpp
+++ src/ladder_main.cpp
@@ -7,12 +7,14 @@ int main() {
     set<string> word_list;
     load_words(word_list, "src/words.txt");
 
-    string begin_word = "Kento";
-    string end_word = "cogs"; 
-\
+    string begin_word = "cat";
+    string end_word = "dog"; 
+
     vector<string> ladder = generate_word_ladder(begin_word, end_word, word_list);
     print_word_ladder(ladder);
     cout << endl;
-    // verify_word_ladder(ladder);
+    cout << ladder.size();
+    cout << endl;
+    verify_word_ladder();
     return 0;
 }
